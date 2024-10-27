dc4023bcc4d9380daedef257f9c99ec5f65e0d35
P2003H <167503094+P2003H@users.noreply.github.com>
2024-10-27 10:57:07 +0530

    Update API.js

--- client/src/services/API.js
+++ client/src/services/API.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "https://bloodbank-zn99.onrender.com/api/v1" });
+const API = axios.create({ baseURL: process.env.REACT_APP_BASEURL });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("token")) {
