9e0c2f5c097439b671d7a2e3fecb526cd2adc9a4
P2003H <167503094+P2003H@users.noreply.github.com>
2024-10-27 14:20:24 +0530

    Update API.js

--- client/src/services/API.js
+++ client/src/services/API.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "https://bloodbank-web-2.onrender.com/api/v1" });
+const API = axios.create({ baseURL: "https://bloodbank-web-quln-edxsq8d77-p2003hs-projects.vercel.app/api/v1" });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("token")) {
